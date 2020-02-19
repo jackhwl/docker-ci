4102bcc686363e1f0b06cad27a344239e33bbb54
jackhwl <jackhwl@hotmail.com>
2020-02-18 20:51:51 -0500

    Alpine Node.js Container

--- complete-intro-containers/alpine-linux/index.js
+++ complete-intro-containers/alpine-linux/index.js
@@ -3,6 +3,6 @@ const http = require("http")
 http.createServer(function(request, response){
     console.log("request received")
     response.end("omg hi", "utf-8")
-}).listen(4000)
+}).listen(4002)
 
 console.log("server started")
--- complete-intro-containers/alpine-linux/readme.md
+++ complete-intro-containers/alpine-linux/readme.md
@@ -1,2 +1,2 @@
 docker build -t my-node-app .     
-docker run --init --rm -p 4044:4000 my-node-app
+docker run --init --rm -it -p 4044:4000 my-node-app whoami
