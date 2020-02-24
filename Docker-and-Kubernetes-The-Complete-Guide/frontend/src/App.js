ff9d0ca1ad59a175e1f0a0ed9744c83d471894b7
jackhwl <jackhwl@hotmail.com>
2020-02-23 20:57:31 -0500

    6.79. Do We Need Copy?

--- Docker-and-Kubernetes-The-Complete-Guide/frontend/src/App.js
+++ Docker-and-Kubernetes-The-Complete-Guide/frontend/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit 33<code>src/App.js</code> and save to reload 123.
+          Edit 22<code>src/App.js</code> and save to reload .
         </p>
         <a
           className="App-link"
